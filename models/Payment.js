d874e20e81137c47ba63cb4949e95b9099b40024
Amir Khan <khansourav119@gmail.com>
2025-12-08 20:08:50 +0600

    payment section

---
+++ models/Payment.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const PaymentSchema = new mongoose.Schema({
+  id: { type: String, required: true, unique: true },
+  issueId: String,
+  payerUid: String,
+  payerEmail: String,
+  amount: Number,
+  currency: String,
+  transactionId: String,
+  status: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Payment", PaymentSchema);
